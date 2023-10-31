59e139a59cf9407b721cb916ad0241b346e61c18
Bryan <bryan.ferrando@epitech.eu>
2023-10-31 09:39:00 +0000

    Marvin

--- Battalion.java
+++ Battalion.java
@@ -5,19 +5,6 @@ public class Battalion {
 
     private List<Character> characters = new ArrayList<>();
 
-    public static void main ( String args []) {
-List < Mage > mages = new ArrayList < >() ;
-mages . add ( new Mage ( " Merlin " ) ) ;
-mages . add ( new Mage ( " Mandrake " ) ) ;
-List < Warrior > warriors = new ArrayList < >() ;
-warriors . add ( new Warrior ( " Spartacus " ) ) ;
-warriors . add ( new Warrior ( " Clovis " ) ) ;
-Battalion battalion = new Battalion () ;
-battalion . add ( mages ) ;
-battalion . add ( warriors ) ;
-battalion . display () ;
-}
-
     public void add(List<? extends Character> characters) {
         this.characters.addAll(characters);
     }
