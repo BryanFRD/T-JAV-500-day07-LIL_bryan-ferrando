39d2871ade16c39ae68a73b434df9860a5655152
Bryan <bryan.ferrando@epitech.eu>
2023-10-31 08:55:44 +0000

    Marvin

---
+++ Battalion.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Battalion {
+
+    List<Character> characters = new ArrayList<>();
+
+    public void add(List<Character> characters) {
+        this.characters.addAll(characters);
+    }
+
+    public void display() {
+        for (Character character : characters) {
+            System.out.println(character.getName());
+        }
+    }
+
+}
